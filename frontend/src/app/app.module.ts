2ed34efffc2218724fc373eb82c04504f0fbc2cd
Godfrey Fernandes <godfrey.f@ET-C02WK5MEG8WN.local>
2019-03-17 02:54:53 +0530

    Added OTP page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,43 +1,37 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { HttpModule } from '@angular/http';
 
-// import { AboutPage } from '../pages/about/about';
-// import { ContactPage } from '../pages/contact/contact';
-// import { HomePage } from '../pages/home/home';
-// import { TabsPage } from '../pages/tabs/tabs';
-import { LoginPage } from '../pages/login/login';
-
-import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { IonicApp, IonicModule } from 'ionic-angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+
+import { MyApp } from './app.component';
 
 @NgModule({
   declarations: [
-    MyApp,
-    LoginPage
-    // AboutPage,
-    // ContactPage,
-    // HomePage,
-    // TabsPage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(MyApp, {
+
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    LoginPage
-    // AboutPage,
-    // ContactPage,
-    // HomePage,
-    // TabsPage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AppRoutingModule,
+    AuthServiceProvider
   ]
 })
 export class AppModule {}
