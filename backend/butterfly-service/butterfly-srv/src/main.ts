874ee2bf304e9f8a426987d23979900b096e30ed
giridharan <giridharan.s@widas.in>
2019-08-17 23:59:57 +0530

    fix: cors issue resolved

--- backend/butterfly-service/butterfly-srv/src/main.ts
+++ backend/butterfly-service/butterfly-srv/src/main.ts
@@ -1,12 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import * as cors from 'cors';
+//import cors from 'cors';
 import { AppModule } from './app.module';
 import { MyLogger } from './logger/MyLogger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('butterfly-srv');
   app.useLogger(MyLogger);
-  app.use(cors());
+ app.enableCors();
+  const options = new DocumentBuilder()
+    .setTitle('Butterfly Services')
+    .setDescription('The butterfly API documentation')
+    .setBasePath('butterfly-srv/')
+    .setVersion('1.0')
+    .addTag('user', 'butterfly user services')
+    .addTag('language', 'butterfly language services')
+    .addTag('category', 'butterfly category services')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+ 
+  SwaggerModule.setup('butterfly-srv/docs', app, document);
+
   await app.listen(3000);
+ 
 }
 bootstrap();
